d921c6867272f974c86480470305cf98219fd371
bit2424 <nelsonquinones2424@gmail.com>
2019-05-23 17:09:12 -0500

    "Avances en Krutskal Matriz de adyacencia"

--- Aplication/src/Estructures/Graphs/AdjacencyMatrixGraph.java
+++ Aplication/src/Estructures/Graphs/AdjacencyMatrixGraph.java
@@ -276,6 +276,28 @@ public class AdjacencyMatrixGraph<V,E extends Comparable<E>> implements IGraph<V
         return result;
     }
 
+    // A class to represent a subset for union-find
+    class subset
+    {
+        int parent, rank;
+    };
+
+
+    // A class to represent a graph edge
+    class Edge implements Comparable<Edge>
+    {
+        int src, dest, weight;
+
+        // Comparator function used for sorting edges
+        // based on their weight
+        public int compareTo(Edge compareEdge)
+        {
+            return this.weight-compareEdge.weight;
+        }
+    };
+
+
+
     //Nelson
     @Override
     public ArrayList<Integer> Kruskal(int startPosition) {
@@ -283,6 +305,43 @@ public class AdjacencyMatrixGraph<V,E extends Comparable<E>> implements IGraph<V
 
         return null;
     }
+
+
+    // A utility function to find set of an element i
+    // (uses path compression technique)
+    int find(subset subsets[], int i)
+    {
+        // find root and make root as parent of i (path compression)
+        if (subsets[i].parent != i)
+            subsets[i].parent = find(subsets, subsets[i].parent);
+
+        return subsets[i].parent;
+    }
+
+    // A function that does union of two sets of x and y
+    // (uses union by rank)
+    void Union(subset subsets[], int x, int y)
+    {
+        int xroot = find(subsets, x);
+        int yroot = find(subsets, y);
+
+        // Attach smaller rank tree under root of high rank tree
+        // (Union by Rank)
+        if (subsets[xroot].rank < subsets[yroot].rank)
+            subsets[xroot].parent = yroot;
+        else if (subsets[xroot].rank > subsets[yroot].rank)
+            subsets[yroot].parent = xroot;
+
+            // If ranks are same, then make one as root and increment
+            // its rank by one
+        else
+        {
+            subsets[yroot].parent = xroot;
+            subsets[xroot].rank++;
+        }
+    }
+
+
 
     //Nelson
     @Override
@@ -403,5 +462,13 @@ public class AdjacencyMatrixGraph<V,E extends Comparable<E>> implements IGraph<V
 
     public void setElementsReference(ArrayList<VertexM<V>> elementsReference) {
         this.elementsReference = elementsReference;
+    }
+
+    public EdgeM[][] getMatrixAdyacency() {
+        return matrixAdyacency;
+    }
+
+    public void setMatrixAdyacency(EdgeM[][] matrixAdyacency) {
+        this.matrixAdyacency = matrixAdyacency;
     }
 }
